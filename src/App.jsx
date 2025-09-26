f8282832f32c0c9c428b7a7f376fae528f78acac
Mehedi Hasan <mehedihasan624358@gmail.com>
2025-09-27 01:31:51 +0600

    nav section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="">
+        <div class="flex flex-col lg:flex-row justify-between items-center px-8 mx-8 py-6 text-black">
+          <h3 class="font-semibold">CS - Ticket System</h3>
+          <div class="flex flex-col lg:flex-row gap-4 text-sm my-8 lg:my-4">
+            <div className='flex flex-col lg:flex-row gap-4 py-2'>
+              <p>Home</p>
+              <p>FAQ</p>
+              <p>ChangeLog</p>
+              <p>Blog</p>
+              <p>Download</p>
+              <p>Contact</p>
+            </div>
+            <button class="bg-gradient-to-r from-purple-700 to-purple-500 text-white px-4 py-2 rounded">+ New Ticket</button>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='bg-gray-100 p-8'>
+        <div className='grid grid-cols-2 gap-8 text-center'>
+          <div className='bg-gradient-to-r from-purple-700 to-purple-500 text-white p-8 rounded '>
+            <h3>In Progess</h3>
+            <p>0</p>
+          </div>
+          <div className='bg-gradient-to-r from-green-700 to-green-500 text-white p-8 rounded'>
+            <h3>Resolved</h3>
+            <p>0</p>
+          </div>
+        </div>
+        <div></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
