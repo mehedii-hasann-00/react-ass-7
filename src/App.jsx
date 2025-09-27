80d5fb077f0227b27766a831952967c6c35bdb1f
Mehedi Hasan <mehedihasan624358@gmail.com>
2025-09-27 13:19:30 +0600

    ticket cards functionality add

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react'
 function App() {
   const [count, setCount] = useState(0);
   const [tickets, setTickets] = useState([]);
+  const [taskInProgess, setTaskInProgess] = useState([]);
+  const [taskResolved, setTaskResolved] = useState([]);
 
   useEffect(() => {
     fetch('/data.json')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setTickets(data)
       })
       .catch((error) => console.error('Error loading JSON data:', error));
@@ -23,12 +24,37 @@ function App() {
     });
   };
 
+  const handle_in_progess = (t) => {
+    if (taskInProgess.length > 0) {
+      let found = false;
+      for (let i = 0; i < taskInProgess.length; i++) {
+
+        if (t.id === taskInProgess[i].id) {
+          found = true;
+        }
+      }
+      if (!found) {
+        setTaskInProgess(prev => [...prev, { id: t.id, title: t.title }])
+      }
+    }
+    else {
+      setTaskInProgess([{ id: t.id, title: t.title }])
+    }
+  }
+
+  const handle_resolve = (task) => {
+    setTickets(tickets.filter(t => t.id !== task.id));
+    setTaskInProgess(taskInProgess.filter(t => t.id !== task.id));
+    setTaskResolved(prev => [...prev, { title: task.title }])
+  }
+
+  console.log(taskInProgess);
   return (
     <>
-      <div class="">
-        <div class="flex flex-col lg:flex-row justify-between items-center px-8 mx-8 py-6 text-black">
-          <h3 class="font-semibold">CS - Ticket System</h3>
-          <div class="flex flex-col lg:flex-row gap-4 text-sm my-8 lg:my-4">
+      <div className="">
+        <div className="flex flex-col lg:flex-row justify-between items-center px-8 mx-8 py-6 text-black">
+          <h3 className="font-semibold">CS - Ticket System</h3>
+          <div className="flex flex-col lg:flex-row gap-4 text-sm my-8 lg:my-4">
             <div className='flex flex-col lg:flex-row gap-4 py-2'>
               <p>Home</p>
               <p>FAQ</p>
@@ -37,7 +63,7 @@ function App() {
               <p>Download</p>
               <p>Contact</p>
             </div>
-            <button class="bg-gradient-to-r from-purple-700 to-purple-500 text-white px-4 py-2 rounded">+ New Ticket</button>
+            <button className="bg-gradient-to-r from-purple-700 to-purple-500 text-white px-4 py-2 rounded">+ New Ticket</button>
           </div>
 
         </div>
@@ -45,12 +71,12 @@ function App() {
       <div className='bg-gray-100 p-8'>
         <div className='grid grid-cols-2 gap-8 text-center'>
           <div className='bg-gradient-to-br from-purple-700 to-purple-500 text-white p-16 rounded'>
-            <h3>In Progess</h3>
-            <p>0</p>
+            <h3 className='text-lg font-semibold'>In Progess</h3>
+            <p className='my-4 text-6xl font-bold'>{taskInProgess.length}</p>
           </div>
           <div className='bg-gradient-to-r from-green-700 to-green-500 text-white p-16 rounded'>
-            <h3>Resolved</h3>
-            <p>0</p>
+            <h3 className='text-lg font-semibold'>Resolved</h3>
+            <p className='my-4 text-6xl font-bold'>{taskResolved.length}</p>
           </div>
         </div>
         <div className='flex my-8 gap-8 py-8'>
@@ -59,33 +85,31 @@ function App() {
             {tickets && tickets.length > 0 ?
               <div className='grid grid-cols-2 gap-4'>
                 {tickets.map(t => (
-                  <div key={t.id} class=" rounded-lg border border-gray-200 shadow-lg p-4 bg-white">
-                    <div class="flex justify-between items-center">
-                      <h2 class="text-lg font-medium text-gray-900">{t.title}</h2>
+                  <div key={t.id} className=" rounded-lg border border-gray-200 shadow-lg p-4 bg-white cursor-pointer" onClick={() => handle_in_progess(t)}>
+                    <div className="flex justify-between items-center">
+                      <h2 className="text-lg font-medium text-gray-900">{t.title}</h2>
                       {t.status && t.status === "Open" ?
-                        <span class="text-green-500 text-sm p-2 bg-green-100 rounded-full flex items-center">
+                        <span className="text-green-500 text-sm p-2 bg-green-100 rounded-full flex items-center">
                           <span className="w-2.5 h-2.5 bg-green-600 rounded-full mr-2"></span>
                           {t.status}
                         </span>
                         :
-                        <span class="text-yellow-500 text-sm p-2 bg-yellow-100 rounded-full flex items-center">
+                        <span className="text-yellow-500 text-sm p-2 bg-yellow-100 rounded-full flex items-center">
                           <span className="w-2.5 h-2.5 bg-yellow-600 rounded-full mr-2"></span>
                           {t.status}
                         </span>
                       }
                     </div>
-
-
-                    <p class="text-gray-600 mt-2 line-clamp-2 text-sm">{t.description}</p>
-                    <div class="mt-4 flex justify-between items-center">
+                    <p className="text-gray-600 mt-2 line-clamp-2 text-sm">{t.description}</p>
+                    <div className="mt-4 flex justify-between items-center">
                       <div>
-                        <span class="text-sm">{t.id}</span>
+                        <span className="text-sm">{t.id}</span>
                         <span className={`${t.priority && t.priority === "High" ? "text-red-600" : t.priority === "Low" ? "text-green-600" : "text-yellow-600"} text-sm mx-4`}>{t.priority} PRIORITY</span>
                       </div>
 
-                      <div class="flex items-center space-x-2">
-                        <span class="text-gray-500 text-sm">{t.customer}</span>
-                        <span class="text-gray-400 text-sm"><span><i className="fa fa-thin fa-calendar pr-2"></i></span>{formatDate(t.createdAt)}</span>
+                      <div className="flex items-center space-x-2">
+                        <span className="text-gray-500 text-sm">{t.customer}</span>
+                        <span className="text-gray-400 text-sm"><span><i className="fa fa-thin fa-calendar pr-2"></i></span>{formatDate(t.createdAt)}</span>
                       </div>
                     </div>
                   </div>
@@ -95,7 +119,33 @@ function App() {
               : null}
           </div>
           <div className='w-1/3'>
-            <h2 className='text-xl font-semibold'>Task Status</h2>
+            <div>
+              <h2 className='text-xl font-semibold'>Task Status</h2>
+              {taskInProgess && taskInProgess.length > 0 ?
+                taskInProgess.map(t => (
+                  <div key={t.id} className='bg-white rounded my-4 p-4 shadow-lg'>
+                    <p className='text-sm'>{t.title}</p>
+                    <button className='w-full bg-green-600 py-1 rounded my-2' onClick={() => handle_resolve(t)}>complete</button>
+                  </div>
+                ))
+                :
+                <p className='text-sm font-light my-4'>Select a ticket to add to Task Status</p>
+              }
+            </div>
+            <div>
+              <h2 className='text-xl font-semibold'>Resolved Task</h2>
+              {taskResolved && taskResolved.length > 0 ?
+                taskResolved.map(t => (
+                  <div key={t.id} className='bg-blue-100 rounded my-4 p-4 shadow-lg'>
+                    <p className='text-sm'>{t.title}</p>
+                  </div>
+                ))
+                :
+                <p className='text-sm font-light my-4'>No resolved tasks yet.</p>
+              }
+              
+            </div>
+
           </div>
         </div>
       </div>
